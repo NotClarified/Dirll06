1461ae557f124bc5fb5befae98e7607076f45b50
JeonDae_HomePC <james911256@gmail.com>
2023-10-01 18:24:56 +0900

    make_arrow함수 구현

--- main.py
+++ main.py
@@ -12,21 +12,30 @@ running = True
 
 character_x, character_y = 0, 0
 point_x, point_y = 0, 0
-save_point = []
+save_pointX = []
+save_pointY = []
+def canvas():
+    clear_canvas()
+    ground.draw(TUK_X // 2, TUK_Y // 2)
 def make_arrow():
-    pass
+    global  save_point
+    canvas()
+    for i in len(save_point):
+        arrow.clip_draw(0, 0, 50, 52, save_pointX(i), save_pointY(i))
+   # update_canvas()
 def character_move():
     pass
 
 def handle_events():
     global  running
-    global  point_x, point_y
+    global  point_x, point_y, save_point
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSE_TOUCHID:
-            save_point.append(event.x, event.y)
+            save_point.append(event.x)
+            save_point.append(event.y)
             make_arrow()
             character_move()
 
