6b996e34a9432ea2770e062cd1f64f4c44a3a01b
JeonDae_HomePC <james911256@gmail.com>
2023-10-01 23:01:08 +0900

    마우스 애니메이션 클릭시에도 유지
    왜 클릭하면 캐릭터가 마우스 커서에 붙어있는것인가?

--- main.py
+++ main.py
@@ -42,32 +42,38 @@ def character_move():
     global  point_x, point_y
     global character_x, character_y, frame
     global running, arrows
-    for i in range(0, 100 + 1, 4):
-        frame = (frame + 1) % 8
-        t = i / 100
-        if not arrows:
-            break
+
+    if len(arrows) > 0:
         target_x, target_y = arrows[0]
-        character_x = (1 - t) * character_x + t * target_x
-        character_y = (1 - t) * character_y + t * target_y
-        if character_x > target_x:
-            character.clip_draw(frame * 100, 10, 100, 80, character_x, character_y)  # left
-        elif character_x == target_x:
-            character.clip_draw(frame * 100, 310, 100, 80, character_x, character_y)  # left_stop
-            del arrows[0]
-            if not arrows:
-                running = False  # arrows가 비어있으면 종료
-                break
-        elif character_x < target_x:
-            character.clip_draw(frame * 100, 110, 100, 80, character_x, character_y)  # right
+        for i in range(0, 100 + 1, 4):
+            frame = (frame + 1) % 8
+            t = i / 100
+            if i == 0:
+                character_x, character_y = 0, 0
+            character_x = (1 - t) * character_x + t * target_x
+            character_y = (1 - t) * character_y + t * target_y
+            ground.draw(TUK_X // 2, TUK_Y // 2)
+            if character_x > target_x:
+                ground.draw(TUK_X // 2, TUK_Y // 2)
+                character.clip_draw(frame * 100, 10, 100, 80, character_x, character_y)  # left
+            elif character_x == target_x:
+                ground.draw(TUK_X // 2, TUK_Y // 2)
+                character.clip_draw(frame * 100, 310, 100, 80, character_x, character_y)  # left_stop
+                character_x, character_y = target_x, target_y
+                arrows.pop(0)
+            elif character_x < target_x:
+                ground.draw(TUK_X // 2, TUK_Y // 2)
+                character.clip_draw(frame * 100, 110, 100, 80, character_x, character_y)  # right
+        for ax, ay in arrows:
+            arrow.clip_draw(0, 0, 50, 52, ax, ay)  # 클릭한 손가락
+        arrow.clip_draw(0, 0, 50, 52, point_x, point_y)  # 움직이는 손가락
         update_canvas()
         delay(0.2)
 
 
 
 while running:
-    clear_canvas()
     ground.draw(TUK_X // 2, TUK_Y // 2)
     mouse_events()
-    handle_events()
     character_move()
+    handle_events()  # 핸들 이벤트를 여기서
