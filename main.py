cee50d20a25eb7db643cd2a363dc4c76f5e0a2e1
JeonDae_HomePC <james911256@gmail.com>
2023-10-01 18:49:14 +0900

    character_move부분 구현

--- main.py
+++ main.py
@@ -24,11 +24,38 @@ def make_arrow():
         arrow.clip_draw(0, 0, 50, 52, save_pointX(i), save_pointY(i))
    # update_canvas()
 def character_move():
-    pass
+    global character_x, character_y, frame
+    global  save_pointX, save_pointY
+    make_arrow()
+    frame = (frame + 1) % 5
+    if (character_x > save_pointX[0]):
+        ground.draw(TUK_X // 2, TUK_Y // 2)
+        character.clip_draw(frame * 100, 10, 100, 80, character_x, character_y)  # left
+        update_canvas()
+    elif (character_x == save_pointX[0]):
+        ground.draw(TUK_X // 2, TUK_Y // 2)
+        character.clip_draw(frame * 100, 310, 100, 80, character_x, character_y)  # left_stop
+        del save_pointX[0]
+        del save_pointY[0]
+        update_canvas()
+    elif (character_x < save_pointX[0]):
+        ground.draw(TUK_X // 2, TUK_Y // 2)
+        arrow.clip_draw(0, 0, 50, 52, point_x, point_y)
+        character.clip_draw(frame * 100, 110, 100, 80, character_x, character_y)  # right
+        update_canvas()
+        if (character_x == save_pointX[0]):
+            ground.draw(TUK_X // 2, TUK_Y // 2)
+            character.clip_draw(frame * 100, 410, 100, 80, character_x, character_y)  # right_stop
+            del save_pointX[0]
+            del save_pointY[0]
+            update_canvas()
+
+
 
 def handle_events():
     global  running
     global  point_x, point_y, save_point
+    global  character_x, character_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -37,11 +64,15 @@ def handle_events():
             save_point.append(event.x)
             save_point.append(event.y)
             make_arrow()
-            character_move()
-
+            for i in range(0, 100 + 1, 4):
+                t = i / 100
+                character_x = (1 - t) * character_x + t * point_x
+                character_y = (1 - t) * character_y + t * point_y
+                character_move()
+                delay(0.2)
 
 while running:
     clear_canvas()
     ground.draw(TUK_X // 2, TUK_Y // 2)
-
-    character_move()
+    update_canvas()
+    handle_events()
