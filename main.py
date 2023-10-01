b0618bcce31b397ab302dffe226c350cc4ee5b0c
JeonDae_HomePC <james911256@gmail.com>
2023-10-01 18:19:41 +0900

    handle_events 구현

--- main.py
+++ main.py
@@ -12,12 +12,27 @@ running = True
 
 character_x, character_y = 0, 0
 point_x, point_y = 0, 0
-
+save_point = []
 def make_arrow():
     pass
 def character_move():
     pass
 
+def handle_events():
+    global  running
+    global  point_x, point_y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSE_TOUCHID:
+            save_point.append(event.x, event.y)
+            make_arrow()
+            character_move()
+
+
 while running:
     clear_canvas()
     ground.draw(TUK_X // 2, TUK_Y // 2)
+
+    character_move()
