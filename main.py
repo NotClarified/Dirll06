a3e9afc980fbceda028ca1837878f09fb5e448dd
JeonDae_HomePC <james911256@gmail.com>
2023-10-01 21:47:34 +0900

    손 커서를 따라 움직이는 모션

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ frame = 0
 running = True
 
 point_x, point_y = 0, 0
+character_x, character_y = 0, 0
 arrows = []
 
 def handle_events():
@@ -25,13 +26,33 @@ def handle_events():
         elif event.type == SDL_MOUSEBUTTONDOWN:
             point_x, point_y = event.x, TUK_Y - 1 - event.y
             arrows.append((point_x, point_y))
+            character_move()
 
 def character_move():
-    global frame
+    global  point_x, point_y
+    global character_x, character_y, frame
+    frame = (frame + 1) % 8
+    clear_canvas()
+    ground.draw(TUK_X // 2, TUK_Y // 2)
+    for i in range(0, 100 + 1, 4):
+        t = i / 100
+        for ax, ay in arrows:
+            arrow.clip_draw(0, 0, 50, 52, ax, ay)
+        character_x = (1 - t) * character_x + t * point_x
+        character_y = (1 - t) * character_y + t * point_y
+        if character_x > point_x:
+            character.clip_draw(frame * 100, 10, 100, 80, character_x, character_y)  # left
+        elif character_x == point_x:
+            character.clip_draw(frame * 100, 310, 100, 80, character_x, character_y)  # left_stop
+            del arrows[0]
+        elif character_x < point_x:
+            character.clip_draw(frame * 100, 110, 100, 80, character_x, character_y)  # right
+        update_canvas()
+        delay(0.05)
+
+
 while running:
     clear_canvas()
     ground.draw(TUK_X // 2, TUK_Y // 2)
     update_canvas()
     handle_events()
-    frame = (frame + 1) % 8
-    delay(0.05)
